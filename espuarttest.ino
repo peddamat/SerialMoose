15f48d3b653337c7cacc3e7925466921451274bc
Sam Peddamatham <peddamat@gmail.com>
2023-01-03 13:15:10 -0800

    Setup sniffer lines.

--- espuarttest.ino
+++ espuarttest.ino
@@ -1,19 +1,27 @@
+
+HardwareSerial &DebugConsole = Serial;
+HardwareSerial &SniffRX = Serial1;
+HardwareSerial &SniffTX = Serial2;
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial2.begin(0, SERIAL_8N1, 34, 33, false, 11000UL);
-  Serial.println("Wooo!");
-  unsigned long detectedBaudRate = Serial2.baudRate();
+
+  DebugConsole.begin(115200);
+
+  SniffRX.begin(0, SERIAL_8N1, 34, 32, false, 11000UL);
+
+  DebugConsole.println("Initialized RX Line.");
+
+  unsigned long detectedBaudRate = SniffRX.baudRate();
   if (detectedBaudRate) {
-    Serial.printf("Detected baudrate is %lu\n", detectedBaudRate);
+    DebugConsole.printf("Detected baudrate is %lu\n", detectedBaudRate);
   } else {
-    Serial.println("No baudrate detected, Serial1 will not work!");
+    DebugConsole.println("No baudrate detected, Serial1 will not work!");
   }
+
+  DebugConsole.println("Initialized TX Line.");
+  SniffTX.begin(detectedBaudRate, SERIAL_8N1, 35, 33, false, 11000UL);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Sent command.");
-  Serial2.println("Hi!");
-  delay(5000);
 }
