10da797cf631d2f5ca576ab9c364e90bbec8de07
Sam Peddamatham <peddamat@gmail.com>
2023-01-05 12:46:19 -0800

    Start refactoring.

--- src/Bluetooth.h
+++ src/Bluetooth.h
@@ -24,3 +24,27 @@ void BTAuthCompleteCallback(boolean success)
     Serial.println("Pairing failed, rejected by user!!");
   }
 }
+
+// Modified from esp32/Print.cpp.  
+static int debug_vprintf(const char *format, va_list ap) {
+
+    char loc_buf[64];
+    char * temp = loc_buf;
+    int len = vsnprintf(temp, sizeof(loc_buf), format, ap);
+
+    if(len < 0) {
+        return 0;
+    };
+    if(len >= (int)sizeof(loc_buf)){  
+        temp = (char*) malloc(len+1);
+        if(temp == NULL) {
+            return 0;
+        }
+        len = vsnprintf(temp, len+1, format, ap);
+    }
+    len = DebugConsole.write((uint8_t*)temp, len);
+    if(temp != loc_buf){
+        free(temp);
+    }
+    return len;
+}
--- src/SerialMoose.ino
+++ src/SerialMoose.ino
@@ -32,91 +32,6 @@ void setup_debug_console() {
 }
 #endif
 
-// Modified from esp32/Print.cpp.  
-static int debug_vprintf(const char *format, va_list ap) {
-
-    char loc_buf[64];
-    char * temp = loc_buf;
-    int len = vsnprintf(temp, sizeof(loc_buf), format, ap);
-
-    if(len < 0) {
-        return 0;
-    };
-    if(len >= (int)sizeof(loc_buf)){  
-        temp = (char*) malloc(len+1);
-        if(temp == NULL) {
-            return 0;
-        }
-        len = vsnprintf(temp, len+1, format, ap);
-    }
-    len = DebugConsole.write((uint8_t*)temp, len);
-    if(temp != loc_buf){
-        free(temp);
-    }
-    return len;
-}
-
-static void rx_task(void *arg) {
-
-  static const char *RX_TASK_TAG = "RX";
-  esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
-  uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
-  while (1) {
-      const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
-      if (rxBytes > 0) {
-          data[rxBytes] = 0;
-          ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_ERROR);
-      }
-  }
-  free(data);
-}
-
-static void tx_task(void *arg) {
-
-  static const char *TX_TASK_TAG = "TX";
-  esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
-  uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
-  while (1) {
-      const int txBytes = uart_read_bytes(UART_NUM_2, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
-      if (txBytes > 0) {
-          data[txBytes] = 0;
-          ESP_LOG_BUFFER_HEXDUMP(TX_TASK_TAG, data, txBytes, ESP_LOG_ERROR);
-      }
-  }
-  free(data);
-}
-
-
-// TODO: Automatically confirming pairing request for now.
-//       Replace with code to read "BOOT" pin button press.
-static void confirm_pin(void *arg) {
-
-  while (1) {
-    if (confirmRequestPending)
-    {
-        DebugConsole.confirmReply(true);
-        DebugConsole.println("Connected");
-        confirmRequestPending = false;
-    }
-    vTaskDelay(10);
-  }
-}
-
-int echo(int argc, char **argv)
-{
-    auto lastArg = argc - 1;
-    for ( int i = 1; i < argc; i++) {
-
-        shell.print(argv[i]);
-
-        if (i < lastArg)
-            shell.print(F(" "));
-    }
-    shell.println();
-
-    return EXIT_SUCCESS;
-}
-
 int badArgCount( char * cmdName )
 {
     shell.print(cmdName);
@@ -185,9 +100,8 @@ int detect_baud(int argc, char **argv) {
 
 }
 
-void setup() {
 
-  pinMode(0, INPUT);
+void setup() {
 
   setup_debug_console();
 
@@ -205,5 +119,51 @@ void setup() {
 
 void loop() {
   shell.executeIfInput();
-
 }
+
+static void rx_task(void *arg) {
+
+  static const char *RX_TASK_TAG = "RX";
+  esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+  uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
+  while (1) {
+      const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+      if (rxBytes > 0) {
+          data[rxBytes] = 0;
+          ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_ERROR);
+      }
+  }
+  free(data);
+}
+
+static void tx_task(void *arg) {
+
+  static const char *TX_TASK_TAG = "TX";
+  esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+  uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
+  while (1) {
+      const int txBytes = uart_read_bytes(UART_NUM_2, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+      if (txBytes > 0) {
+          data[txBytes] = 0;
+          ESP_LOG_BUFFER_HEXDUMP(TX_TASK_TAG, data, txBytes, ESP_LOG_ERROR);
+      }
+  }
+  free(data);
+}
+
+
+// TODO: Automatically confirming pairing request for now.
+//       Replace with code to read "BOOT" pin button press.
+static void confirm_pin(void *arg) {
+
+  while (1) {
+    if (confirmRequestPending)
+    {
+        DebugConsole.confirmReply(true);
+        DebugConsole.println("Connected");
+        confirmRequestPending = false;
+    }
+    vTaskDelay(10);
+  }
+}
+
