46bf3a2331c03cd042f0dc7b1372fb256b616092
Sam Peddamatham <peddamat@gmail.com>
2023-01-09 22:55:10 -0800

    Baud rate detection working.

--- src/cmd_sniffer.c
+++ src/cmd_sniffer.c
@@ -9,8 +9,10 @@
 #include "esp_log.h"
 #include "esp_rom_gpio.h"
 #include "esp_vfs_fat.h"
+#include "hal/uart_ll.h"
 #include "main.h"
 #include "sdkconfig.h"
+#include "soc/rtc.h"
 #include "soc/uart_periph.h"
 
 static const char *TAG = "cmd_sniffer";
@@ -92,11 +94,17 @@ static void probe2_rxpin_task(void *arg) {
 // }
 
 static void probe1_txpin_task(void *arg) {
-    static const char *TX_TASK_TAG = "TX_TASK";
-    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+    esp_log_level_set("P1_TASK", ESP_LOG_INFO);
     while (1) {
         uart_write_bytes(PROBE1_UART_CHANNEL, "helloworld", strlen("helloworld"));
-        // sendData(TX_TASK_TAG, "Hello world");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void probe2_txpin_task(void *arg) {
+    esp_log_level_set("P2_TASK", ESP_LOG_INFO);
+    while (1) {
+        uart_write_bytes(PROBE2_UART_CHANNEL, "yellowmellow", strlen("yellowmellow"));
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
@@ -115,16 +123,18 @@ static int do_generate_traffic_cmd(int argc, char **argv) {
 
     int len = gen_traffic_args.pin->count;
 
-    if (len == 1) {
-        int pin = gen_traffic_args.pin->ival[0];
+    // Configure Probe 1 TX pin and start task
+    int pin1 = gen_traffic_args.pin->ival[0];
+    ESP_ERROR_CHECK(uart_set_pin(PROBE1_UART_CHANNEL, pin1, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    xTaskCreate(probe1_txpin_task, "probe1_txpin_task", 1024 * 4, NULL, configMAX_PRIORITIES - 1, &Probe1TxTaskHandle);
 
-        // Configure Probe 1 TX pin
-        // configure_sniffer_line(PROBE1_UART_CHANNEL, PROBE1_UART_RX_PIN, pin, UARTS_BAUD_RATE);
-        ESP_ERROR_CHECK(uart_set_pin(PROBE1_UART_CHANNEL, pin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-
-        // Start Probe 1 TX task
-        xTaskCreate(probe1_txpin_task, "probe1_txpin_task", 1024 * 4, NULL, configMAX_PRIORITIES - 1, &Probe1TxTaskHandle);
+    if (len == 2) {
+        // Configure Probe 2 TX pin and start task
+        int pin2 = gen_traffic_args.pin->ival[1];
+        ESP_ERROR_CHECK(uart_set_pin(PROBE2_UART_CHANNEL, pin2, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+        xTaskCreate(probe2_txpin_task, "probe2_txpin_task", 1024 * 4, NULL, configMAX_PRIORITIES - 1, &Probe2TxTaskHandle);
     }
+
     // connect_uarts();
     return 0;
 }
@@ -145,12 +155,65 @@ static void register_gen_traffic_cmd(void) {
 // Command: Determine Baud Rate
 /******************************************************************************/
 
+static uint32_t calculateApb(rtc_cpu_freq_config_t *conf) {
+#if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3
+    return APB_CLK_FREQ;
+#else
+    if (conf->freq_mhz >= 80) {
+        return 80 * MHZ;
+    }
+    return (conf->source_freq_mhz * MHZ) / conf->div;
+#endif
+}
+
+uint32_t getApbFrequency() {
+    rtc_cpu_freq_config_t conf;
+    rtc_clk_cpu_freq_get_config(&conf);
+    return calculateApb(&conf);
+}
+
+// Borrowed from Arduino_ESP32
+static unsigned long detect_baud_rate(int channel, bool flg) {
+    uart_dev_t *hw = UART_LL_GET_HW(channel);
+    hw->auto_baud.glitch_filt = 0x08;
+    hw->auto_baud.en = 0;
+    hw->auto_baud.en = 1;
+
+#ifndef CONFIG_IDF_TARGET_ESP32S3
+    while (hw->rxd_cnt.edge_cnt < 30) {  // UART_PULSE_NUM(uart_num)
+        if (flg) return 0;
+        ets_delay_us(1000);
+    }
+
+    // TODO: Fix these mutexes
+    // UART_MUTEX_LOCK();
+    // log_i("lowpulse_min_cnt = %d hightpulse_min_cnt = %d", hw->lowpulse.min_cnt, hw->highpulse.min_cnt);
+    unsigned long divisor = ((hw->lowpulse.min_cnt + hw->highpulse.min_cnt) >> 1);
+    // UART_MUTEX_UNLOCK();
+
+    unsigned long baudrate = getApbFrequency() / divisor;
+
+    return baudrate;
+#else
+    return 0;
+#endif
+}
+
 static struct {
     struct arg_int *pin;
     struct arg_end *end;
 } det_baud_args;
 
 static int do_determine_baud_cmd(int argc, char **argv) {
+    int nerrors = arg_parse(argc, argv, (void **)&det_baud_args);
+    if (nerrors != 0) {
+        arg_print_errors(stderr, det_baud_args.end, argv[0]);
+        return 0;
+    }
+
+    unsigned long baud_rate = detect_baud_rate(PROBE1_UART_CHANNEL, false);
+    printf("Detected baud rate on pin x: %lu\n", baud_rate);
+
     return 0;
 }
 
@@ -184,9 +247,22 @@ static int do_stop_cmd(int argc, char **argv) {
     // TODO: Ensure sniffer has been started...
 
     printf("Stopping sniff!\n\n");
-    vTaskDelete(Probe1RxTaskHandle);
-    vTaskDelete(Probe2RxTaskHandle);
-    vTaskDelete(Probe1TxTaskHandle);
+    if (Probe1RxTaskHandle != NULL) {
+        vTaskDelete(Probe1RxTaskHandle);
+        Probe1RxTaskHandle = NULL;
+    }
+    if (Probe2RxTaskHandle != NULL) {
+        vTaskDelete(Probe2RxTaskHandle);
+        Probe2RxTaskHandle = NULL;
+    }
+    if (Probe1TxTaskHandle != NULL) {
+        vTaskDelete(Probe1TxTaskHandle);
+        Probe1TxTaskHandle = NULL;
+    }
+    if (Probe2TxTaskHandle != NULL) {
+        vTaskDelete(Probe2TxTaskHandle);
+        Probe2TxTaskHandle = NULL;
+    }
 
     return 0;
 }
@@ -219,9 +295,14 @@ static int do_start_cmd(int argc, char **argv) {
 
     // TODO: Ensure sniffer lines are configured...
 
-    printf("Starting sniff!\n\n");
-    xTaskCreate(probe1_rxpin_task, "uart_rx_task", 1024 * 4, NULL, configMAX_PRIORITIES - 1, &Probe1RxTaskHandle);
-    xTaskCreate(probe2_rxpin_task, "uart_tx_task", 1024 * 4, NULL, configMAX_PRIORITIES - 1, &Probe2RxTaskHandle);
+    if (uart_is_driver_installed(PROBE1_UART_CHANNEL)) {
+        xTaskCreate(probe1_rxpin_task, "probe1_rx_task", 1024 * 4, NULL, configMAX_PRIORITIES - 1, &Probe1RxTaskHandle);
+        printf("Started probe 1 sniffer!\n");
+    }
+    if (uart_is_driver_installed(PROBE2_UART_CHANNEL)) {
+        xTaskCreate(probe2_rxpin_task, "probe2_rx_task", 1024 * 4, NULL, configMAX_PRIORITIES - 1, &Probe2RxTaskHandle);
+        printf("Started probe 2 sniffer!\n");
+    }
 
     return 0;
 }
@@ -251,12 +332,14 @@ static void configure_sniffer_line(int channel, int rx_pin, int tx_pin, int baud
         .source_clk = UART_SCLK_APB,
     };
 
-    ESP_ERROR_CHECK(uart_driver_install(channel, READ_BUF_SIZE * 2, 0, 0, NULL, 0));
+    if (!uart_is_driver_installed(channel)) {
+        ESP_ERROR_CHECK(uart_driver_install(channel, READ_BUF_SIZE * 2, 0, 0, NULL, 0));
+    }
     ESP_ERROR_CHECK(uart_param_config(channel, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(channel, tx_pin, rx_pin,
                                  UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 
-    printf("Line configured on pin %i @ %i\n", rx_pin, baud_rate);
+    printf("Probe %i configured on pin %i @ %i baud\n", channel, rx_pin, baud_rate);
 }
 
 /******************************************************************************/
@@ -278,19 +361,29 @@ static int do_setup_cmd(int argc, char **argv) {
     }
 
     int p1_pin = setup_args.p1_pin->ival[0];
-    int p2_pin = setup_args.p2_pin->ival[0];
     int baud = setup_args.baud->ival[0];
 
     configure_sniffer_line(PROBE1_UART_CHANNEL, p1_pin, UART_PIN_NO_CHANGE, baud);
-    configure_sniffer_line(PROBE2_UART_CHANNEL, p2_pin, UART_PIN_NO_CHANGE, baud);
+
+    if (setup_args.p2_pin->count == 1) {
+        int p2_pin = setup_args.p2_pin->ival[0];
+
+        // If only one baud rate was provided, reuse it
+        if (setup_args.baud->count == 1) {
+            configure_sniffer_line(PROBE2_UART_CHANNEL, p2_pin, UART_PIN_NO_CHANGE, baud);
+        } else {
+            int baud2 = setup_args.baud->ival[1];
+            configure_sniffer_line(PROBE2_UART_CHANNEL, p2_pin, UART_PIN_NO_CHANGE, baud2);
+        }
+    }
 
     return 0;
 }
 
 static void register_setup(void) {
-    setup_args.p1_pin = arg_int1("r", "p1", "<p1 pin>", "Specify the Probe 1 pin");
-    setup_args.p2_pin = arg_int1("t", "p2", "<p2 pin>", "Specify the Probe 2 pin");
-    setup_args.baud = arg_int1("b", "baud", "<baud rate>", "Specify the baud rate");
+    setup_args.p1_pin = arg_int1("1", "probe1", "<p1 pin>", "Specify the Probe 1 pin");
+    setup_args.p2_pin = arg_int1("2", "probe2", "<p2 pin>", "Specify the Probe 2 pin");
+    setup_args.baud = arg_intn("b", "baud", "<baud rate>", 1, 2, "Specify the baud rate");
     setup_args.end = arg_end(3);
     const esp_console_cmd_t setup_cmd = {
         .command = "setup",
